4773e6b24ff9d0a78234ddd765129544d1a8677c
maxedin <edinarkhov@gmail.com>
2014-06-21 11:11:19 +1200

    init

---
+++ GooglePasswordReset/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace GooglePasswordReset
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelNewPwd = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.tbNewPassword = new System.Windows.Forms.TextBox();
+            this.tbOldPassword = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(529, 367);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "4) Click to start reset cycle";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(13, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 33);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "3) How many times do you want \r\n     to reset password?";
+            // 
+            // labelNewPwd
+            // 
+            this.labelNewPwd.AutoSize = true;
+            this.labelNewPwd.Location = new System.Drawing.Point(13, 227);
+            this.labelNewPwd.Name = "labelNewPwd";
+            this.labelNewPwd.Size = new System.Drawing.Size(134, 13);
+            this.labelNewPwd.TabIndex = 8;
+            this.labelNewPwd.Text = "Here is your new password";
+            this.labelNewPwd.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2) Your current password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1) Login in Google";
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(29, 186);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 5;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(29, 135);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(147, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // tbNewPassword
+            // 
+            this.tbNewPassword.Location = new System.Drawing.Point(29, 243);
+            this.tbNewPassword.Name = "tbNewPassword";
+            this.tbNewPassword.Size = new System.Drawing.Size(147, 20);
+            this.tbNewPassword.TabIndex = 3;
+            this.tbNewPassword.Visible = false;
+            // 
+            // tbOldPassword
+            // 
+            this.tbOldPassword.Location = new System.Drawing.Point(29, 61);
+            this.tbOldPassword.Name = "tbOldPassword";
+            this.tbOldPassword.Size = new System.Drawing.Size(147, 20);
+            this.tbOldPassword.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.btStart);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.tbNewPassword);
+            this.panel1.Controls.Add(this.labelNewPwd);
+            this.panel1.Controls.Add(this.tbOldPassword);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(529, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(199, 367);
+            this.panel1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 367);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox tbOldPassword;
+        private System.Windows.Forms.TextBox tbNewPassword;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelNewPwd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ GooglePasswordReset/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GooglePasswordReset
+{
+    public partial class Form1 : Form
+    {
+        private string _pswTempalate;
+        private bool _doReset = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Navigate("https://www.google.com/accounts/b/0/EditPasswd");
+        }
+
+
+        private void ResetPassword()
+        {
+            if (numericUpDown1.Value <= 0)
+            {
+                _doReset = false;
+                //tbNewPassword.Visible = labelNewPwd.Visible = false;
+                return;
+            }
+            HtmlDocument document = webBrowser1.Document;
+            HtmlElement element = null;
+
+            element = document.GetElementById("OldPasswd");
+            element.SetAttribute("value", tbOldPassword.Text);
+
+            element = document.GetElementById("Passwd");
+            element.SetAttribute("value", tbNewPassword.Text);
+
+            element = document.GetElementById("PasswdAgain");
+            element.SetAttribute("value", tbNewPassword.Text);
+
+            element = document.GetElementById("save"); 
+            element.InvokeMember("click");
+
+            tbOldPassword.Text = tbNewPassword.Text;
+            numericUpDown1.Value--;
+            tbNewPassword.Text = _pswTempalate + numericUpDown1.Value.ToString();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (!_doReset)
+                return;
+            if (e.Url.AbsoluteUri == "https://accounts.google.com/b/0/EditPasswd")
+            {
+                ResetPassword();
+            }
+            if (e.Url.AbsoluteUri == "https://www.google.com/settings/#")
+            {
+                webBrowser1.Navigate("https://www.google.com/accounts/b/0/EditPasswd");
+            }
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            //tbNewPassword.Visible = labelNewPwd.Visible = true;
+            _pswTempalate = tbOldPassword.Text + "XXX";
+            _doReset = true;
+            tbNewPassword.Text = _pswTempalate + numericUpDown1.Value.ToString();
+            webBrowser1.Navigate("https://www.google.com/accounts/b/0/EditPasswd");
+        }
+    }
+}
